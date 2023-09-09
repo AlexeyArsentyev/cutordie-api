794977b18f42443a5adc830b9b0b5d154d4f4cf0
AlexeyArsentyev <alexey10.arsentyev@gmail.com>
2023-09-09 19:33:18 +0300

    no protect on courses

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -12,8 +12,8 @@ router.post(
 
 router
   .route("/")
-  .get(authController.protect, courseController.getAllCourses)
-  .post(courseController.createCourse);
+  .get(getAllCourses)
+  .post(authController.courseController.createCourse);
 
 router
   .route("/:id")
@@ -21,7 +21,7 @@ router
   .patch(courseController.updateCourse)
   .delete(
     authController.protect,
-    authController.restrictTo,
+    authController.restrictTo("admin"),
     courseController.deleteCourse
   );
 
