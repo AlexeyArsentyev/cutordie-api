7c02faf26a0cbcf894af47a7954fc4e01083a9f5
AlexeyArsentyev <alexey10.arsentyev@gmail.com>
2024-02-06 21:03:59 +0200

    packs schema

---
+++ models/packModel.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+//somecomment
+
+const packSchema = new mongoose.Schema({
+  en: {
+    name: {
+      type: String,
+      required: [true, "pack name required"],
+      unique: [true, "pack unique name required"],
+      trim: true,
+      maxlength: [100, "pack name must have less than 100 characters"],
+      minlength: [2, "pack name must have at least 2 characters"]
+    },
+    description: {
+      type: String,
+      required: [true, "pack description required"]
+    }
+  },
+  ua: {
+    name: {
+      type: String,
+      required: [true, "pack name required"],
+      unique: [true, "pack unique name required"],
+      trim: true,
+      maxlength: [100, "pack name must have less than 100 characters"],
+      minlength: [2, "pack name must have at least 2 characters"]
+    },
+    description: {
+      type: String,
+      required: [true, "pack description required"]
+    }
+  },
+
+  courses: [{ type: String }],
+
+  price: {
+    type: Number,
+    required: [true, "pack price required"],
+    min: [0, "Price must be above equal or above 0"]
+  },
+
+  fileId: {
+    type: String,
+    required: [true, "fileId required"]
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+const Pack = mongoose.model("Pack", packSchema);
+
+module.exports = Course;
