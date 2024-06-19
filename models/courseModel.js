ec8c04588b5007181169d7a4cc0117350037e5a1
AlexeyArsentyev <alexey10.arsentyev@gmail.com>
2024-06-19 14:06:07 +0300

    subheader added

--- models/courseModel.js
+++ models/courseModel.js
@@ -12,6 +12,14 @@ const courseSchema = new mongoose.Schema({
       maxlength: [100, "course name must have less than 100 characters"],
       minlength: [2, "course name must have at least 2 characters"]
     },
+    subHeader: {
+      type: String,
+      required: [true, "course subHeader required"],
+      unique: [true, "course unique subHeader required"],
+      trim: true,
+      maxlength: [100, "course subHeader must have less than 100 characters"],
+      minlength: [2, "course subHeader must have at least 2 characters"]
+    },
     description: {
       type: String,
       required: [true, "course description required"]
